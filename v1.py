07712a0e63e18cc02b1cfbf2dce0e75106ef0092
Vishruth Devan <vishruthdevan@gmail.com>
2021-08-11 20:23:22 +0530

    Change script to download comics between two dates

--- v1.py
+++ v1.py
@@ -10,31 +10,40 @@ def prerequesites():
 
 
 def getComic():
-
-    url = prerequesites()
+    url, endurl = prerequesites()
+    
     while not url.endswith('#'):
         page = requests.get(url)
         page.raise_for_status()
 
         html = bs4.BeautifulSoup(page.text, 'html.parser')
-        comicElement = html.select('a > picture > img[class="lazyload img-fluid"]')
+        comicElement = html.select('img')
 
-        if len(comicElement)==0:
+        if len(comicElement) == 0:
             print("An error occurred, the comic wasn't found.")
         else:
-            comicUrl = comicElement[0].get('src')
+            comicUrl = comicElement[4].get('src')
             comicFile = requests.get(comicUrl)
-            imageFile = open(f"comics/{url[34:38]}-{url[39:41]}-{url[42:44]}.jpg", 'wb')
+            imageFile = open(
+                f"comics/{url[34:38]}-{url[39:41]}-{url[42:44]}.jpg", 'wb')
             print("Downloading from", url)
             for i in comicFile.iter_content(100000):
                 imageFile.write(i)
             imageFile.close()
-  
-        prevComic = html.select('a[class="fa btn btn-outline-secondary btn-circle fa-caret-left sm js-previous-comic"]')
-        if len(prevComic)==0:
-            print("Previous comic not found!")
+
+        if url == endurl:
+            print("Go check out /comics to be amused by the brilliance of Garfield!")
             break
-        url = "https://www.gocomics.com" + prevComic[0].get("href")
+
+        nextComic = html.select(
+            'a[class="fa btn btn-outline-secondary btn-circle fa-caret-right sm"]')
+        if len(nextComic) == 0:
+            print("Bro you can't time travel, neither can I. ")
+            print("Go check out /comics to be amused by the brilliance of Garfield.")
+            break
+
+        url = "https://www.gocomics.com" + nextComic[0].get("href")
+
 
 
 if __name__ == '__main__':

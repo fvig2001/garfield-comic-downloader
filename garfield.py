71fc364bf7dd5520d96069f23edfab3a9b5ef6df
Vishruth Devan <vishruthdevan@gmail.com>
2021-08-12 22:51:26 +0530

    Modify script to download comics between two dates

--- garfield.py
+++ garfield.py
@@ -1,6 +1,8 @@
 import requests, bs4, os
 from datetime import date
 
+message = "Go check out /comics to find the downloaded comic strips!"
+
 def prerequesites():
     top_url = "https://www.gocomics.com/garfield/"
     startDate = input(
@@ -10,52 +12,52 @@ def prerequesites():
     return top_url + startDate, top_url + endDate
 
 
-def get_html(url):
-    page = requests.get(url)
+def get_html():
+    page = requests.get()
     page.raise_for_status()
     html = bs4.BeautifulSoup(page.text, 'html.parser')
     return html
 
 
-def getComic():
-    url, endurl = prerequesites()
-    
+def download_comic():
+    comicElement = get_html().select('img')
+    comicUrl = comicElement[4].get('src')
+    comicFile = requests.get(comicUrl)
+    imageFile = open(
+        f"comics/{url[34:38]}-{url[39:41]}-{url[42:44]}.jpg", 'wb')
+    print("Downloading from", url)
+    for i in comicFile.iter_content(100000):
+        imageFile.write(i)
+    imageFile.close()
+
+
+def next_comic():
+    nextComic = get_html().select(
+        'a[class="fa btn btn-outline-secondary btn-circle fa-caret-right sm"]')
+    global url
+    url = "https://www.gocomics.com" + nextComic[0].get("href")
+
+
+def get_all_comics():
     while not url.endswith('#'):
-        page = requests.get(url)
-        page.raise_for_status()
-
-        html = get_html(url)
-        comicElement = html.select('img')
-
-        if len(comicElement) == 0:
+        try:
+            download_comic()
+        except IndexError:
             print("An error occurred, the comic wasn't found.")
-        else:
-            comicUrl = comicElement[4].get('src')
-            comicFile = requests.get(comicUrl)
-            imageFile = open(
-                f"comics/{url[34:38]}-{url[39:41]}-{url[42:44]}.jpg", 'wb')
-            print("Downloading from", url)
-            for i in comicFile.iter_content(100000):
-                imageFile.write(i)
-            imageFile.close()
 
         if url == endurl:
-            print("Go check out /comics to be amused by the brilliance of Garfield!")
+            print(message)
             break
-
-        nextComic = html.select(
-            'a[class="fa btn btn-outline-secondary btn-circle fa-caret-right sm"]')
-        if len(nextComic) == 0:
-            print("Bro you can't time travel, neither can I. ")
-            print("Go check out /comics to be amused by the brilliance of Garfield.")
+        try:
+            next_comic()
+        except IndexError:
+            print("The comic on that date doesn't exist! ")
+            print(message)
             break
-
-        url = "https://www.gocomics.com" + nextComic[0].get("href")
-
 
 
 if __name__ == '__main__':
     try:
-        getComic()
+        get_all_comics()
     except KeyboardInterrupt:
         print("You have the aborted the process with Ctrl+C")

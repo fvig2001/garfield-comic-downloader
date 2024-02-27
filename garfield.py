438ee7042c6a73d7b5fea3cd4dd6e6ba7e59eb6a
fvig2001 <31123547+fvig2001@users.noreply.github.com>
2024-02-28 07:37:48 +0800

    Update correct file

--- garfield.py
+++ garfield.py
@@ -19,10 +19,12 @@ r"""
 import requests
 import bs4
 import os
+import sys
 
 message = "Go check out /comics to find the downloaded comic strips!"
 html = ''
-
+lastHTML = ""
+lastFile = ""
 
 def prerequesites():
     top_url = "https://www.gocomics.com/garfield/"
@@ -32,6 +34,12 @@ def prerequesites():
     os.makedirs('comics', exist_ok=True)
     return top_url + startDate, top_url + endDate
 
+def prerequesites_2(cusDate):
+    top_url = "https://www.gocomics.com/garfield/"
+    startDate = cusDate.replace("-","/")
+    endDate = startDate
+    os.makedirs('comics', exist_ok=True)
+    return top_url + startDate, top_url + endDate
 
 def get_html():
     page = requests.get(url)
@@ -42,15 +50,24 @@ def get_html():
 
 
 def download_comic(html):
-    comicElement = html.select('img')
-    comicUrl = comicElement[4].get('src')
-    comicFile = requests.get(comicUrl)
-    imageFile = open(
-        f"comics/{url[34:38]}-{url[39:41]}-{url[42:44]}.jpg", 'wb')
-    print("Downloading from", url)
-    for i in comicFile.iter_content(100000):
-        imageFile.write(i)
-    imageFile.close()
+    global lastFile
+    Invalid = True
+    while Invalid == True:    
+        lastFile= f"comics/{url[34:38]}-{url[39:41]}-{url[42:44]}.jpg"
+        comicElement = html.select('img')
+        comicUrl = comicElement[4].get('src')
+        comicFile = requests.get(comicUrl)
+        imageFile = open(lastFile, 'wb')
+        print("Downloading from", url)
+
+    
+        for i in comicFile.iter_content(100000):
+            imageFile.write(i)
+        imageFile.close()
+        if os.path.getsize(lastFile) > 5000: #Check that it is greater than 5 KB as each comic is usually 40KB at least
+            Invalid = False
+        else:
+            print("Retry Downloading from", url)        
 
 
 def next_comic(html):
@@ -61,11 +78,16 @@ def next_comic(html):
 
 
 def get_all_comics():
+    global lastHTML
+    err = False
     while not url.endswith('#'):
         html = get_html()
+        lastHTML = html
         try:
+            err = False
             download_comic(html)
         except IndexError:
+            err = True
             print("An error occurred, the comic wasn't found.")
 
         if url == endurl:
@@ -80,8 +102,12 @@ def get_all_comics():
 
 
 if __name__ == '__main__':
-    try:
-        url, endurl = prerequesites()
+    n = len(sys.argv)
+    try:    
+        if n > 1:
+            url, endurl = prerequesites_2(sys.argv[1])
+        else:
+            url, endurl = prerequesites()
         get_all_comics()
     except KeyboardInterrupt:
         print("You have the aborted the process with Ctrl+C")
